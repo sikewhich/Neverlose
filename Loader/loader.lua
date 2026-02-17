c21a1337d46ec1566c3ed7355279d304da9f619d
sikewhic <turbotext6@gmail.com>
2026-02-17 12:30:04 +0700

    Update loader.lua

--- Loader/loader.lua
+++ Loader/loader.lua
@@ -1,1 +1,6 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/sikewhich/Neverlose/refs/heads/main/Loader/loader.main"))() 
+local config = {
+	ids = {2788229376, 0}, 
+	link = ""
+}
+
+local _a,_b,_c,_d=game:GetService("TweenService"),game:GetService("Lighting"),game:GetService("Players").LocalPlayer,game:GetService("StarterGui")local _e=Instance.new("BlurEffect",_b)_e.Size=0;local _f=Instance.new("ScreenGui",_c:WaitForChild("PlayerGui"))_f.IgnoreGuiInset=true;local _g=Instance.new("TextLabel",_f)_g.Size=UDim2.new(0,800,0,150)_g.Position=UDim2.new(0.5,0,0.5,0)_g.AnchorPoint=Vector2.new(0.5,0.5)_g.BackgroundTransparency=1;_g.Font=Enum.Font.GothamBold;_g.TextColor3=Color3.fromRGB(255,255,255)_g.TextSize=120;_g.TextTransparency=1;_g.Text="Bunni"local _h=TweenInfo.new(1.2,Enum.EasingStyle.Quint,Enum.EasingDirection.Out)_d:SetCore("SendNotification",{Title="Bunni",Text="Checking...",Duration=4})_a:Create(_e,_h,{Size=25}):Play()_a:Create(_g,_h,{TextTransparency=0}):Play()task.wait(3)local _i=os.clock()while os.clock()-_i<1 do end;local _j=false;for _,v in pairs(config.ids)do if game.PlaceId==v then _j=true;break end end;if _j then _g.Text="Supported"_g.TextColor3=Color3.fromRGB(0,255,120)task.spawn(function()loadstring(game:HttpGet(config.link))()end)task.wait(1.5)else _g.Text="Not Supported"_g.TextColor3=Color3.fromRGB(255,50,50)_g.TextSize=90;task.wait(2)end;_a:Create(_g,_h,{TextTransparency=1}):Play()_a:Create(_e,_h,{Size=0}):Play()task.wait(1.2)_f:Destroy()_e:Destroy()
